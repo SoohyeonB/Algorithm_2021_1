6b027481f44a425f7029655c8947edf12b66b1f6
Soohyeon B <56013677+SoohyeonB@users.noreply.github.com>
2021-04-22 15:39:16 +0900

    Update 7. 합병정렬.c

--- 7. 합병정렬.c
+++ 7. 합병정렬.c
@@ -22,18 +22,18 @@ void merge(int list[], int left, int m, int right) {
 	else
 		for (l = i; l <= m; l++)
 			sorted[k++] = list[l];
-	for (l = left; l <= right; l++)
+	for (l = left; l <= right; l++) //임시에서 최종 리스트로 복사
 		list[l] = sorted[l];
 
 }
 
 void merge_sort(int list[], int left, int right) {
 	int m;
-	if (left < right) {
-		m = (left + right) / 2;
-		merge_sort(list, left, m);
-		merge_sort(list, m+1, MAX_SIZE);
-		merge(list, left, m, right);
+	if (left < right) { //분할 가능하면
+			m = (left + right) / 2;//분할
+		merge_sort(list, left, m);  //재귀
+		merge_sort(list, m+1, right);
+		merge(list, left, m, right);  //통치
 	}
 }
 
@@ -44,20 +44,30 @@ void main() {
 
 	int list[MAX_SIZE];
 	int j;
+//리스트 만들기
+//얘는 앞뒤만 다르면 되니까 중복 발생 가능
 	for (int i = 0; i < MAX_SIZE; i++) {
 		list[i] = rand() % 100;
 		j = i-1;
 		if (list[i] == list[j])
 			i--;
 	}
-
+//얘는 모든 원소를 다 비교하니까 중복 발생 불가능
+for (int i = 0; i < MAX_SIZE; i++) 
+   {
+      list[i] = rand() % 100;
+      for (int j = 0; j < i; j++)
+         if (list[i] == list[j])
+            i--;   //중복 없는 배열
+   }
+//리스트 출력하기
 	for (int i = 0; i < MAX_SIZE; i++)
 		printf("[%d] ", list[i]);
 	printf("\n\n"); getch();
-
+//합병정렬
 	merge_sort(list, 0, MAX_SIZE - 1);
-
+//리스트 출력하기
 	for (int i = 0; i < MAX_SIZE; i++)
-		printf("[%d] ", sorted[i]);
+		printf("[%d] ", list[i]);
 	printf("\n\n"); getch();
 }
